a87f09ac3814163875565751a17fc4dd8785e5c0
divineProjects <divinejigu@gmail.com>
2025-03-18 02:14:41 +0200

    updated sql

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,5 +1,5 @@
 -- Insert the following new record to the account table
-INSERT INTO account (
+INSERT INTO  public.account (
         account_firstname,
         account_lastname,
         account_email,
@@ -12,15 +12,15 @@ VALUES (
         'Iam1ronM@n'
     );
 -- Modify the Tony Stark record to change the account_type to "Admin"
-UPDATE account
+UPDATE  public.account
 SET account_type = 'Admin'
 WHERE account_id = 1;
 -- Delete the Tony Stark record from the database.
-DELETE FROM account
+DELETE FROM  public.account
 WHERE account_id = 1;
 
 -- Modify the "GM Hummer" record to read "a huge interior" rather than "small interiors"
-UPDATE inventory
+UPDATE  public.inventory
 SET inv_description = REPLACE (
         inv_description,
         'small interiors',
