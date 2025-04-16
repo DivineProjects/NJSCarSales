ed6cffc73f2ea14d6dc9c791725429e8edb40d4e
divineProjects <divinejigu@gmail.com>
2025-04-16 03:57:12 +0200

    week 05 update

--- server.js
+++ server.js
@@ -43,6 +43,8 @@ app.use(function(req, res, next){
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser())
+app.use(utilities.checkJWTToken)
+
 
 /* ***********************
  * View Egine and Templates
